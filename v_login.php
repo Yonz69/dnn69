adebf17660545ef96c57257d64096190127f7dd1
Yonz_ajaa69 <160454140+Yonz69@users.noreply.github.com>
2024-02-27 11:00:05 +0700

    Add files via upload

---
+++ v_login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Login - Aplikasi Kasir</title>
+</head>
+<body>
+     <h1>APLIKASI KASIR</h1>
+     <form action="m_login.php" method="post">
+          <table>
+               <tr>
+                    <td>Username</td>
+                    <td>:</td>
+                    <td><input type="text" name="username_login" id=""></td>
+               </tr>
+               <tr>
+                    <td>password</td>
+                    <td>:</td>
+                    <td><input type="password" name="password_login" id=""></td>
+               </tr>
+               <tr>
+                    <td></td>
+                    <td></td>
+                    <td><input type="submit" value="login"></td>
+               </tr>
+          </table>
+     </form>
+</body>
+</html>
